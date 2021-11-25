f7b9289b487543dbc5ca3f88ae3206834a45306f
Guilherme1-jpg <henriqueoliveiragui18@gmail.com>
2021-11-24 22:59:23 -0300

    Subindo o projeto todo

---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import {Item} from '../types/Item';
+
+export const items:Item[] = [
+    {date:new Date(2021, 9, 15), category:'food', title:'Mcdonalds', value:32.12},
+    {date:new Date(2021, 9, 15), category:'food', title:'BurguerKing', value:28},
+    {date:new Date(2021, 9, 19), category:'rent', title:'Aluguel casa', value:2300},
+    {date:new Date(2021, 7, 26), category:'salary', title:'Salario', value:4500}
+];
